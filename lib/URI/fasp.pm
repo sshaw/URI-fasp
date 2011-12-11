eca9b37c9186b1b82374c43032471a17edd4d9fd
sshaw <sshaw@lucas.cis.temple.edu>
2011-12-10 21:49:36 -0800

    fasp.pm changes, MANIFEST, ...

--- lib/URI/fasp.pm
+++ lib/URI/fasp.pm
@@ -1,7 +1,5 @@
 package URI::fasp;
 
-#https://support.asperasoft.com/entries/20153151-http-fallback-configuration-testing-and-troubleshooting
-
 use strict;
 use warnings;
 use base 'URI::ssh';
@@ -19,26 +17,21 @@ our $VERSION = '0.01';
  }
 }
 
-sub _init
-{
-    my $class = shift;
-    my $self  = $class->SUPER::_init(@_);    
-    $self->faspport($self->default_faspport) unless defined $self->faspport;
-    # Other defaults...
-    $self;
-}
+sub _init {  shift->SUPER::_init(@_); }
+sub default_fasp_port { 33001 }
 
-sub default_faspport { 33001 }
-
-sub faspport
+# Aspera uses "port". To avoid conflicting with the URI method of the same name we call 
+# this method "fasp_port". Both names are supported as query params.
+sub fasp_port
 {
-    my $self = shift;
-    return $self->query_param('port', @_) if @_;
+    my $self  = shift;
+    if(@_) {
+      $self->query_param('port', @_);
+      return;
+    }
     
-    # No need to entertain a list 
-    my $port = $self->query_param('port') || 
-      	       $self->query_param('faspport');
-    $port;
+    $self->query_param('port') or
+    $self->default_fasp_port;
 }
 
 sub as_ssh
@@ -64,17 +57,55 @@ URI::fasp - URI handler for Aspera's FASP protocol
    $fasp->targetrate(10_000);
 
    print $fasp->port;		# 97001
-   print $fasp->faspport;	# 33001
+   print $fasp->fasp_port;	# 33001
    print $fasp->bwcap;		# 25000
    # ...
    $ssh = $fasp->as_ssh;	# URI::ssh 
    print $ssh->port;		# 97001	
 
+=head1 DESCRIPTION
+
+Aspera uses seperate control and a data connections. The control connection is a SSH session. 
+This class is a subclass of L<< C<URI::ssh>|URI >> and uses the L<< C<URI::QueryParam> >> mixin. 
+
 =head1 METHODS
 
+=head2 C<as_ssh>
 
-       
+L<< C<URI::ssh>|URI >> representation of the instance's control connection
+
+=head2 C<default_fasp_port>
+
+The default FASP port is 33001
+
+=head2 C<fasp_port>
+
+Defaults to L<< C<default_fasp_port> >>
+
+=head2 C<bwcap>
+
+Bandwidth cap
+
+=head2 C<policy>
+
+Transfer policy
+
+=head2 C<httpport>
+
+Fallback HTTP port
+
+=head2 C<targetrate>
+
+Target transfer rate
+
 =head1 SEE ALSO
 
-L<URI>, http://asperasoft.com
+L<URI>, L<URI::QueryParam>, http://asperasoft.com
 
+=head1 AUTHOR
+
+Skye Shaw (sshaw AT lucas.cis.temple.edu)
+
+=head1 LICENSE
+
+Copyright (c) 2011 Skye Shaw. This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
--- t/fasp.t
+++ t/fasp.t
@@ -2,23 +2,29 @@ use strict;
 use warnings;
 
 use URI;
-use Test::More tests => 17;
+use Test::More tests => 19;
 
 my $uri = URI->new('fasp://example.com');
 isa_ok($uri, 'URI::fasp');
 is($uri->port, 22);
-is($uri->default_faspport, 33001);
-is($uri->faspport, 33001);
+is($uri->default_fasp_port, 33001);
+is($uri->fasp_port, 33001);
+is("$uri", 'fasp://example.com');
+
+# Like URI we only show the port if it's explicitly set
+$uri->fasp_port(33001);
+is("$uri", 'fasp://example.com?port=33001');
 
 $uri = URI->new('fasp://example.com:33001?port=5000&bwcap=1000&policy=fair&httpport=8080&targetrate=50000');
-is($uri->faspport, 5000);
+is($uri->fasp_port, 5000);
+is($uri->port, 33001);
 is($uri->bwcap, 1000);
 is($uri->policy, 'fair');
 is($uri->httpport, 8080);
 is($uri->targetrate, 50000);
 
-$uri->faspport(33001);
-is($uri->faspport, 33001);
+$uri->fasp_port(33001);
+is($uri->fasp_port, 33001);
 
 $uri->bwcap(25000);
 is($uri->bwcap, 25000);
@@ -34,7 +40,5 @@ isa_ok($ssh, 'URI::ssh');
 is($ssh->port, 33001);
 is($ssh->query, undef);
 
-# Aspera uses "port", we use that and "faspport"
-$uri = URI->new('fasp://example.com:33001?faspport=5000');
-is($uri->faspport, 5000);
+
 
